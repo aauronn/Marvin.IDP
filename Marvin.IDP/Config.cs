0825fbec363f42d8478ad8b341c0aced06677fe6
Luis Gutierrez <aauronn@gmail.com>
2019-07-08 10:53:16 -0400

    Add project files.

---
+++ Marvin.IDP/Config.cs
@@ -0,0 +1,62 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "59C7B7BC-E81A-46C7-A471-0F7A2C4E0FB5",
+                    Username = "Frank",
+                    Password= "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                },
+
+                new TestUser
+                {
+                    SubjectId = "59C7B7BC-E81A-46C7-A471-0F7A2C4E0FB6",
+                    Username = "Claire",
+                    Password= "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                }
+            };
+        }
+
+
+        // Identity-related resources (scopes)
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
